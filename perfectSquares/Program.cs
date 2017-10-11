14a09419ff98ae8768eddda2a9e52ad44a544ba0
Samuel Faris <samuelfaris@gmail.com>
2017-10-11 16:26:55 -0700

    Second commit

---
+++ perfectSquares/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perfectSquares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(int i in perfectSquare())
+            {
+                Console.WriteLine(i);
+                
+            }
+            Console.Read();
+        }
+
+        public static IEnumerable<int> perfectSquare()
+        {
+            int i = 1;
+            int square;
+            int sum = 0;
+            while(true)
+            {
+                if (sum >= 10000) break;
+                square = i * i;
+                sum += square;
+                i++;
+                yield return sum;
+            }
+        }
+    }
+}
